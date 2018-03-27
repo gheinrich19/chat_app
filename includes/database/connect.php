37316661f48765ae9e1e0115ec61110d1ab5f744
Gage Heinrich <gage.heinrich@aggiemail.usu.edu>
2018-03-26 18:18:54 -0600

    mod pass

--- includes/database/connect.php
+++ includes/database/connect.php
@@ -2,7 +2,7 @@
 
     $db_host = 'localhost';
     $db_user = 'root';
-    $db_passowrd = 'football19';
+    $db_passowrd = '**********';
     $db_name = 'chat';
 
     if($connection = mysqli_connect($db_host, $db_user, $db_passowrd,$db_name)){
